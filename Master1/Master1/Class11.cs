fcfe87272640e38f6d1372be7cda8486de448f74
Opilane TTHK <opilane@tthk.ee>
2020-11-30 18:01:07 +0200

    Add project files.

---
+++ Master1/Master1/Class11.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Master1
+{
+    class Class11
+    {
+    }
+}
---
+++ Master1/Master1/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Master1
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            profileimage.Source = ImageSource.FromFile("AboutMe.jpg");
+            aboutlist.ItemsSource = GetMenuList();
+            var homePage = typeof(Views.AboutPage);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenuItems> GetMenuList()
+        {
+            var list = new List<MasterMenuItems>();
+            //---------------------------------------------------------------------------------------
+
+            list.Add(new MasterMenuItems()
+            {
+                Text = "My Info",
+                Detail = "Info",
+                ImagePath = "info.png",
+                TargetPage = typeof(Views.AboutPage)
+            });
+            //---------------------------------------------------------------------------------------
+
+            list.Add(new MasterMenuItems()
+            {
+                Text = "My experience",
+                Detail = "Experience",
+                ImagePath = "practice.png",
+                TargetPage = typeof(Views.ExperiencePage)
+            });
+            //---------------------------------------------------------------------------------------
+
+            list.Add(new MasterMenuItems()
+            {
+                Text = "My skills",
+                Detail = "Skills",
+                ImagePath = "notebook.png",
+                TargetPage = typeof(Views.SkillsPage)
+            });
+            //---------------------------------------------------------------------------------------
+            list.Add(new MasterMenuItems()
+            {
+                Text = "My trophy",
+                Detail = "Trophy",
+                ImagePath = "trophy.png",
+                TargetPage = typeof(Views.SkillsPage)
+            });
+            //---------------------------------------------------------------------------------------
+            return list;
+        }
+
+        private void aboutlist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenuItem = (MasterMenuItems)e.SelectedItem;
+            Type selectedPage = selectedMenuItem.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+            IsPresented = false;
+        }
+    }
+}
